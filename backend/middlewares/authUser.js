e4943396148ce771ecde65232e00aaebd12a9025
ankit <generalankit51@gmail.com>
2025-03-07 19:53:41 +0545

    Fixed the issue of token not being generated when user logs in

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export const authUser = (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1]; // Get token from Bearer header
+        const token = req.headers.authorization?.split(' ')[1];
 
         if (!token) {
             return res.json({ success: false, message: "Not Authorized, Login again!" })
